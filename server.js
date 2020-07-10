5c7e1f9b0cedc21e6cf7ebf7c5471c8e8c7cb720
Ryan Geddes <rgeddes09@gmail.com>
2020-07-09 20:55:30 -0700

    constructor issues

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 const cors = require('cors');
 const morgan = require('morgan');
+const { response } = require('express');
 
 const app = express();
 
@@ -39,15 +40,47 @@ app.use(handleError);
 // ROUTE HANDLER FUNCTIONS
 // ----------------------------------------------
 
+function handleSearch (req,res) {
+    let bookObjArr = [];
+    let title = req.query.title;
+    let author = req.query.author;
+    const API = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}&key=AIzaSyD0kg3D3tRoFiFU7V5h8BWaHlEPY1oGyUU`
+    console.log(`API call: ${API}`)
+    superagent.get(API)
+        .then(obj =>{
+            console.log(`object.items line 50: ${obj.body.items}`);
+            obj.body.items.forEach(book =>{
+                let bookObj = new Book(book);
+                bookObjArr.push(bookObj);
+                console.log(bookObj);
+            })
+        response.status(200).json(bookObjArr);
+        console.log(`book object array line 56: ${bookObjArr}`);
+        })
+        .catch(error => {
+            console.log(`error with Bookhandler: ${error}`)
+            response.status(500).send(error);
+        });
+}
+
+
+function Book(obj) {
+    this.description= obj.volumeInfo.description;
+    // this.author= obj.volumeInfo.authors;
+    this.title = obj.volumeInfo.title;
+    // this.isbn = obj.industryIdentifiers[0].identifier;
+    // this.thumbnail = obj.imageLinks.thumbnail;
+  }
+
 function registerForm (req,res) {
     res.render('pages/searches/new');
 }
 
-function handleSearch(req,res){
-    console.log(req.query);
-    res.send(`Form with GET ... ${req.query.title}`)
-    //res.render('pages/searches/new', { formdata: req.query });
-}
+// function handleSearch(req,res){
+//     console.log(req.query);
+//     res.send(`Form with GET ... ${req.query.title}`)
+//     //res.render('pages/searches/new', { formdata: req.query });
+// }
 
 
 function helloHandler(req, res){
